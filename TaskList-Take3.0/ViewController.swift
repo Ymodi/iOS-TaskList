e1b7bc94eb2df6c3e29d9b341b1556651d449278
Yesha Modi <yesha@Yeshas-MacBook-Pro.local>
2017-02-18 10:14:21 -0500

    Add delete functionality and Important task colour changed

--- TaskList-Take3.0/ViewController.swift
+++ TaskList-Take3.0/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     
     var data : [Task] = []
+//    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//    let task = Task(context: context)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,8 +43,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             print ("Yesh it is!")
             
             //Why I cant see smiley there
-            cell.textLabel?.text = "🤓\(task.name!)"
-        }else{
+            //cell.textLabel?.text = "🤓 \(task.name!)"
+            cell.textLabel?.textColor = UIColor.blue
+         }else{
          print ("not fetching data")
         }
         cell.textLabel?.text = task.name!
@@ -60,6 +63,57 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
 
-
+    //var deletePlanetIndexPath: NSIndexPath? = nil
+    var delete: IndexPath? = nil
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if editingStyle == .delete {
+        
+            //deletePlanetIndexPath = indexPath
+//            let dataToDelete = data[indexPath.row]
+//            confirmDelete(data: dataToDelete)
+//            
+            let task = data[indexPath.row]
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do {
+                data = try context.fetch(Task.fetchRequest())
+            }
+            catch {
+                print("Fetching Failed")
+            }
+        }
+        tableView.reloadData()
+    }
+    
+//    
+//    func confirmDelete(data: Task) {
+//        
+//         let actionSheetController = UIAlertController(title: "Delete data", message: "Are you sure you want to permanently delete?", preferredStyle: .actionSheet)
+//        
+//        let yesButton = UIAlertAction(title: "Delete", style: .destructive, handler: {
+//            
+//        })
+//        
+//        let noButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { { action in self.showMyAlert(alertTitle: "Negative", alertMessage: "You missed it!") }})
+//        
+//        actionSheetController.addAction(yesButton)
+//        actionSheetController.addAction(noButton)
+//        //need to present the button or alert
+//        
+//        present(actionSheetController, animated: true, completion: nil)
+//    }
+    
+//    func handleDeleteData(alertAction: UIAlertAction!) -> Void {
+//        print ("Will work")
+//        if let indexPath
+//    }
+//    
+//    func handleCancelData(alertAction: UIAlertAction!) {
+//        //delete = nil
+//        // deletePlanetIndexPath = nil
+//    }
 }
 
